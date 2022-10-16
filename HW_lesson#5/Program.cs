302d88afb9b766ed5bca797b85e3225b4d82d151
Nikita Gutenev <gutenev229@gmail.com>
2022-10-16 15:04:01 +0300

    сделал вторую задачу

--- HW_lesson#5/Program.cs
+++ HW_lesson#5/Program.cs
@@ -21,6 +21,33 @@
 // }
 
 
-//task 2
+// //task 2
+// int quantity = new Random().Next(1, 10);
+// int[] randarr = GetRandomArr(quantity);
+// Console.WriteLine($"Kолличество элементов в массиве: {quantity}");
+// Console.WriteLine($"Массив с рандомными числами: [{String.Join(", ", randarr)}], сумма чисел на нечетных позициях: {sum_uneven(randarr)}");
+// int[] GetRandomArr(int quan)
+// {
+//     int[] result = new int[quan];
+//     for (int i=0; i<quan;i++)
+//     {
+//         result[i] = new Random().Next(20);
+//     }
+//     return result;
+// }
+// int sum_uneven (int[] arr)
+// {
+//     int result = 0;
+//     for (int i = 0; i<arr.Length; i++)
+//     {
+//         if (i%2 != 0)
+//         {
+//             result+= arr[i];
+//         }
+//     }
+//     return result;
+// }
 
+
+//task 3
 
