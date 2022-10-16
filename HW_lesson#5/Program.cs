84fe2f76bde626e92f3e7a50bd6f312874290edd
Nikita Gutenev <gutenev229@gmail.com>
2022-10-16 14:46:26 +0300

    сделал первую задачу

--- HW_lesson#5/Program.cs
+++ HW_lesson#5/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// //task 1
+// int[] randarr = GetRandomArr();
+// Console.WriteLine($"Массив с рандомными трёхзначными числами: [{String.Join(", ", randarr)}], колличество четных чисел: {even_numbers(randarr)}");
+// int[] GetRandomArr()
+// {
+//     int[] result = new int[15];
+//     for (int i=0; i<15;i++)
+//     {
+//         result[i] = new Random().Next(100,1000);
+//     }
+//     return result;
+// }
+// int even_numbers (int[] arr)
+// {
+//     int result = 0;
+//     for (int i=0; i<15;i++)
+//     {
+//         if (arr[i] % 2 == 0) result++;
+//     }
+//     return result;
+// }
+
+
+//task 2
+
+
