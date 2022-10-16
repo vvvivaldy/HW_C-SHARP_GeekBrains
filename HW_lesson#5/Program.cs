7c87cb7818eca0b697daec01c4c2ddd8765479fa
Nikita Gutenev <gutenev229@gmail.com>
2022-10-16 15:13:57 +0300

    сделал третью задачу

--- HW_lesson#5/Program.cs
+++ HW_lesson#5/Program.cs
@@ -49,5 +49,43 @@
 // }
 
 
-//task 3
+// //task 3
+// int quantity = new Random().Next(2, 11);
+// int[] randarr = GetRandomArr(quantity);
+// Console.WriteLine($"Kолличество элементов в массиве: {quantity}");
+// Console.WriteLine($"Массив с рандомными вещественными числами: [{String.Join(", ", randarr)}], разница между максимальным и минимальным элементов массива: {MaxValue(randarr)-MinValue(randarr)}");
+// int[] GetRandomArr(int quan)
+// {
+//     int[] result = new int[quan];
+//     for (int i=0; i<quan;i++)
+//     {
+//         result[i] = new Random().Next(-100, 101);
+//     }
+//     return result;
+// }
+// int MaxValue (int[] arr)
+// {
+//     int MaxValue = arr[0];
 
+//     for (int i = 1; i<arr.Length; i++)
+//     {
+//         if (arr[i] > MaxValue)
+//         {
+//             MaxValue = arr[i];
+//         }
+//     }
+//     return MaxValue;
+// }
+// int MinValue (int[] arr)
+// {
+//     int MinValue = arr[0];
+
+//     for (int i = 1; i<arr.Length; i++)
+//     {
+//         if (arr[i] < MinValue)
+//         {
+//             MinValue = arr[i];
+//         }
+//     }
+//     return MinValue;
+// }
