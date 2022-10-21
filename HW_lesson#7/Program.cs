91852185d4ab9a558676aba250d87f0769b873ae
Nikita Gutenev <gutenev229@gmail.com>
2022-10-21 22:15:18 +0300

    сделал третью задачу

--- HW_lesson#7/Program.cs
+++ HW_lesson#7/Program.cs
@@ -83,62 +83,59 @@
 
 
 
-//task 3
-//ввод данных
-Console.Write("Введите размерность массива (стороки): ");
-int line = int.Parse(Console.ReadLine()!);
-Console.Write("Введите размерность массива (столбцы): ");
-int col = int.Parse(Console.ReadLine()!);
+// //task 3
+// //ввод данных
+// Console.Write("Введите размерность массива (стороки): ");
+// int line = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите размерность массива (столбцы): ");
+// int col = int.Parse(Console.ReadLine()!);
 
-int[,] arr = GetArray(line, col);
+// int[,] arr = GetArray(line, col);
 
-// вывод данных
-PrintArray(arr);
-Console.WriteLine();
-double[] arithmeticMEAN = Get_arithmetic_mean(arr, line, col); 
-Console.WriteLine($"Среднее арифметическое столбцов: {String.Join("; ", arithmeticMEAN)}");
+// // вывод данных
+// PrintArray(arr);
+// Console.WriteLine();
+// double[] arithmeticMEAN = Get_arithmetic_mean(arr, line, col); 
+// Console.WriteLine($"Среднее арифметическое столбцов: {String.Join("; ", arithmeticMEAN)}");
 
 
 
-// функции
-int[,] GetArray(int line, int col)
-{
-    int[,] result = new int[line,col];
-    for (int i = 0; i < line; i++)
-    {
-        for (int j = 0; j<col; j++)
-        {
-            result[i,j] = new Random().Next(0,11);
-        }
-    }
-    return result;
-}
-void PrintArray (int[,] Array)
-{
-    for (int i =0;i<Array.GetLength(0);i++)
-    {
-        for (int j =0;j<Array.GetLength(1);j++)
-        {
-            Console.Write($"{Array[i,j]} ");
-        }
-        Console.WriteLine();
-    }
-}
+// // функции
+// int[,] GetArray(int line, int col)
+// {
+//     int[,] result = new int[line,col];
+//     for (int i = 0; i < line; i++)
+//     {
+//         for (int j = 0; j<col; j++)
+//         {
+//             result[i,j] = new Random().Next(0,11);
+//         }
+//     }
+//     return result;
+// }
+// void PrintArray (int[,] Array)
+// {
+//     for (int i =0;i<Array.GetLength(0);i++)
+//     {
+//         for (int j =0;j<Array.GetLength(1);j++)
+//         {
+//             Console.Write($"{Array[i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
-double[] Get_arithmetic_mean (int[,] Arr, int line, int column)
-{
-    double round;
-    double[] result = new double[column];
-    for (int j = 0; j<column; j++)
-    {
-        for (int i=0;i<line;i++)
-        {
-            result[j] += Arr[i,j];
-        }
-        result[j] /= line;
-        round = result[j];
-        Math.Round (round, 2);
-        result[j] = round;
-    }
-    return result;
-}
+// double[] Get_arithmetic_mean (int[,] Arr, int line, int column)
+// {
+//     double[] result = new double[column];
+//     for (int j = 0; j<column; j++)
+//     {
+//         for (int i=0;i<line;i++)
+//         {
+//             result[j] += Arr[i,j];
+//         }
+//         result[j] /= line;
+//         Math.Floor(result[j]);
+//     }
+//     return result;
+// }
