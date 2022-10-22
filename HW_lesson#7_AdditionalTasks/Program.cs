7dffa0cba45aa105b5baf5e23e74545a947bdca3
Nikita Gutenev <gutenev229@gmail.com>
2022-10-22 21:22:16 +0300

    Сделал пол задачи номер 2

--- HW_lesson#7_AdditionalTasks/Program.cs
+++ HW_lesson#7_AdditionalTasks/Program.cs
@@ -69,3 +69,49 @@
 // 2-nd Addition Task
 
 
+// немного перевыполню задачу и для красоты спирально заполню любой массив квадратного размера с четными сторонами и числами начиная со 100
+// на листочке находил закономерности,так что имею право :D
+// Console.Write("Введите размер стороны квадратного массива (четное число): ");
+// int size = int.Parse(Console.Readline()!);
+int size = 6;
+int[,] arr = new int [size,size];
+
+// заполним первую строку массива
+for (int i =0; i<size; i++)
+{
+    arr[0,i]=100 + i;
+}
+
+// а дальше магия)
+int n = size; // переменная n создана исключительно для меня, т.к. на бумаге у меня обозначены позиции с помощью n
+int line = 0;
+int col = n-1;
+int current = arr[0,n-1] +1; // потому что  первая строка массива заполнена. Это актуальное число
+while (n>0)
+{
+    for (int i = line+1; i<n; i++){
+        arr[i,col] = current;
+        current++;
+        line = i;
+    }
+    for (int j = col-1; j>=0; j--){
+        arr[line,j] = current;
+        current++;
+        col=j;
+    }
+    n--;
+}
+
+
+
+
+
+// вывод массива
+for (int i =0;i<size;i++)
+{
+    for (int j=0;j<size;j++)
+    {
+        Console.Write($"{arr[i,j]} ");
+    }
+    Console.WriteLine();
+}
