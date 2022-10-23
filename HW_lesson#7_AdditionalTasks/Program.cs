e2c23ed535e90a6b0eddeb117f845682e16c69ce
Nikita Gutenev <gutenev229@gmail.com>
2022-10-23 10:05:50 +0300

    небольшие поправки по оформлению второй задачи

--- HW_lesson#7_AdditionalTasks/Program.cs
+++ HW_lesson#7_AdditionalTasks/Program.cs
@@ -130,8 +130,9 @@ for (int i =0;i<size;i++)
 {
     for (int j=0;j<size;j++)
     {
-        Console.Write($"{arr[i,j]} ");
+        Console.Write($"{arr[i,j]}  ");
     }
+    Console.WriteLine();
     Console.WriteLine();
 }
 
