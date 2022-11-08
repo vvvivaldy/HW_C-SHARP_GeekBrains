51cec645f5894063f7ca29b544cc765a37f8fb4f
Nikita Gutenev <gutenev229@gmail.com>
2022-11-08 20:54:09 +0300

    метод №1

--- HW_lesson#8/Program.cs
+++ HW_lesson#8/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// задачи №60 и №62 написаны в папке HW_lesson#7_AdditionalTasks
+
+Console.Write("введите размерность массива: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write(" ");
+int b = int.Parse(Console.ReadLine()!);
+
+WriteArr(GetArr(a,b));
+
+Console.Write("Измененный массив: ");
+
+int[,] SortDecreaseArr (int[,] arr)
+{
+    int max;
+    int temp;
+    for (int i = 0; i<arr.GetLenth(0); i++)
+    {
+        max = arr[i,0];
+        for (int j = 1;j<arr.GetLenth(1);j++)
+        {
+            if (max < arr[i,j])
+            {
+                arr[]
+                
+            }
+        }
+    }
+    return arr;
+}
+
+int[,] GetArr(int a, int b)
+{
+    int[,] result = new int[a, b];
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            result[i, j] = new Random().Next(10);
+        }
+    }
+    return result;
+}
+
+void WriteArr (int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
