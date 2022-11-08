782a9a03420e58544638410a974104d1baed0763
Nikita Gutenev <gutenev229@gmail.com>
2022-11-08 23:14:43 +0300

    сделал три задачи + еще 2 в другой папке

--- HW_lesson#8/Program.cs
+++ HW_lesson#8/Program.cs
@@ -1,4 +1,4 @@
-﻿// задачи №60 и №62 написаны в папке HW_lesson#7_AdditionalTasks
+// задачи №60 и №62 написаны в папке HW_lesson#7_AdditionalTasks
 
 // // номер 1
 // Console.Write("введите размерность массива: ");
@@ -99,6 +99,68 @@
 //         }
 //     }
 //     return count;
+// }
+
+// int[,] GetArr(int a, int b)
+// {
+//     int[,] result = new int[a, b];
+//     for (int i = 0; i < a; i++)
+//     {
+//         for (int j = 0; j < b; j++)
+//         {
+//             result[i, j] = new Random().Next(10);
+//         }
+//     }
+//     return result;
+// }
+
+// void WriteArr (int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             Console.Write($"{arr[i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+//номер 3 ---------- ЗАМЕНИЛ ЗАДАЧУ №58 НА РЕКОМЕНДОВАННУЮ
+
+// Console.Write("введите размерность массива: ");
+// int a = int.Parse(Console.ReadLine()!);
+// Console.Write(" ");
+// int b = int.Parse(Console.ReadLine()!);
+
+
+// int[,] arr = GetArr(a,b);
+// WriteArr(arr);
+
+// Console.WriteLine("Измененный массив: ");
+
+// WriteArr(SortDecreaseArr(arr));
+
+// int[,] SortDecreaseArr (int[,] arr)
+// {
+//     int temp;
+//     for (int j = 1; j<arr.GetLength(1); j+=2)
+//     {
+//         for (int i = 0;i<arr.GetLength(0);i++) 
+//         {
+//             for (int h = i+1 ;h<arr.GetLength(0); h++) 
+//             {
+//                 if (arr[i,j] > arr[h,j])
+//                 {
+//                     temp = arr[i,j];
+//                     arr[i,j] = arr[h,j];
+//                     arr[h,j] = temp;
+//                 }
+//             }
+//         }
+//     }
+//     return arr;
 // }
 
 // int[,] GetArr(int a, int b)
